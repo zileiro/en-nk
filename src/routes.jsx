551a98b1b74a2b66b562c0138983143e5ade22b6
zileiro <gbfc@discente.ifpe.edu.br>
2022-12-04 18:38:24 -0300

    Adicionando react-router no projeto, configurando as rotas e modificando o app.jsx

---
+++ src/routes.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Links from './pages/Links'
+import Error from './pages/Error'
+
+function RoutesApp(){//componentes de rotas
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/links" element={<Links/>}/>
+                <Route path="*" element={ <Error/> }/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default RoutesApp;
